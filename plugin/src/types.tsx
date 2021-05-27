7b4f8bbe13d89f288e6c8e141a04924f85fd0dbe
Harold Martin <harold.martin@gmail.com>
2021-05-27 16:22:44 -0700

    add copyright headers

--- plugin/src/types.tsx
+++ plugin/src/types.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2021 Harold Martin.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import {DataTableColumn} from "flipper-plugin";
 
 export type Rows = Record<string, Array<Record<string, any>>>;
